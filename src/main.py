e9d403041650f97f794fb12f8c4d84fda42123b7
MartinFiorde <fiorde_90@hotmail.com>
2024-11-21 22:56:41 -0300

    feat: 3 window

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
+from src.models.window import Window
+
+
 def main():
     print("hello word")
     win = Window(800, 600)
+    win.wait_for_close()
 
 
 if __name__ == "__main__":
---
+++ src/models/window.py
@@ -0,0 +1,25 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze solver")
+        self.canvas = Canvas(self.__root, width=width, height=height)
+        self.canvas.pack(fill=BOTH, expand=True)
+        self.running = True
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+        
+    def wait_for_close(self):
+        while self.running:
+            self.redraw()
+            
+    def close(self):
+        self.running = False
+        
+    def draw_line(self, line, fill_collor):
+        line.draw(self.canvas, fill_collor)
+        
