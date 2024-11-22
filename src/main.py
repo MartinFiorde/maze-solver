3231a175c3b9c7639a24c6d599c025f6707472e3
MartinFiorde <fiorde_90@hotmail.com>
2024-11-21 23:32:53 -0300

    feat: 6 cell move

--- src/main.py
+++ src/main.py
@@ -7,14 +7,15 @@ from src.models.cell import Cell
 def main():
     print("hello word")
     win = Window(800, 600)
-    win.draw_line(Line(Point(10, 20), Point(30, 40)), "red")
     win.draw_line(Line(Point(2, 2), Point(2, 598)), "black")
     win.draw_line(Line(Point(2, 2), Point(798, 2)), "black")
     win.draw_line(Line(Point(798, 2), Point(798, 598)), "black")
     win.draw_line(Line(Point(2, 598), Point(798, 598)), "black")
-    win.draw_line(Line(Point(10, 40), Point(20, 30)), "blue")
-    cell = Cell(Point(20,20), Point(40,40), True, True, True, True)
-    win.draw_cell(cell, "black")
+    cell1 = Cell(Point(20,20), Point(40,40), True, False, True, True)
+    cell2 = Cell(Point(40,20), Point(60,40), True, True, True, False)
+    win.draw_cell(cell1, "black")
+    win.draw_cell(cell2, "Blue")
+    cell1.draw_move(cell2)
     win.wait_for_close()
 
 
--- src/models/cell.py
+++ src/models/cell.py
@@ -11,9 +11,11 @@ class Cell:
         self._y1 = p1.y
         self._y2 = p2.y
         self._win = False
+        self.canvas = None
 
         
     def draw(self, canvas: Canvas, fill_color):
+        self.canvas = canvas
         if self.has_top_wall:
             canvas.create_line(self._x1, self._y1, self._x2, self._y1, fill=fill_color, width=2)
         if self.has_right_wall:
@@ -29,3 +31,13 @@ class Cell:
             
     def close(self):
         self.running = False
+        
+    def draw_move(self, to_cell: "Cell", undo=False):
+        fill_collor = "gray" if undo else "red"
+        x1 = (self._x1 + self._x2) / 2
+        y1 = (self._y1 + self._y2) / 2
+        x2 = (to_cell._x1 + to_cell._x2) / 2
+        y2 = (to_cell._y1 + to_cell._y2) / 2
+        
+        self.canvas.create_line(x1, y1, x2, y2, fill=fill_collor, width=2)
+        
