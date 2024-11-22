e4c882134ff42354f435715e5b0c10846d107bbb
MartinFiorde <fiorde_90@hotmail.com>
2024-11-21 22:55:39 -0300

    feat: proyect base

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # maze-solver
+
+[Link to bootdev course](https://www.boot.dev/courses/build-maze-solver-python)
---
+++ src/main.py
@@ -0,0 +1,7 @@
+def main():
+    print("hello word")
+    win = Window(800, 600)
+
+
+if __name__ == "__main__":
+    main()
