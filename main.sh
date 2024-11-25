9a75ba1ae636dd484b5efe026d4ec9853414a0cf
MartinFiorde <fiorde_90@hotmail.com>
2024-11-25 17:02:24 -0300

    feat: add seed support to replicate maze generation and solve pathing

--- main.sh
+++ main.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-python3 -m src.main
+
+# Solicita al usuario las semillas opcionales
+read -p "Enter seed1 (or press Enter to use default): " seed1
+read -p "Enter seed2 (or press Enter to use default): " seed2
+
+# Construye el comando con los valores proporcionados
+command="python3 -m src.main"
+
+# Agrega las semillas solo si se ingresaron valores
+if [ -n "$seed1" ]; then
+    command+=" $seed1"
+fi
+
+if [ -n "$seed2" ]; then
+    command+=" $seed2"
+fi
+
+# Ejecuta el comando
+echo "Executing: $command"
+eval $command
+
+echo "Script done. Press Enter to close..."
+read
--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
+import argparse
+
 from src.models.window import Window
 from src.models.maze import Maze
 
 
-def main():
+def main(seed1, seed2):
     win = Window(800, 600)
-    maze = Maze(win, 50, 50, 10, 14, 50, 50)
-    maze.solve()
+    maze = Maze(win, 50, 50, 10, 14, 50, 50, seed1)
+    maze.solve(seed2)
     win.wait_for_close()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Execute the script with seeds to replicate random values")
+    parser.add_argument("seed1", nargs="?", type=int, default=None, help="Seed for maze generator (optional).")
+    parser.add_argument("seed2", nargs="?", type=int, default=None, help="seed for maze solver (optional).")
+
+    args = parser.parse_args()
+    main(args.seed1, args.seed2)
