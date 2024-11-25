91488e4cc85682ea2232e38b3b4f46fac2126927
MartinFiorde <fiorde_90@hotmail.com>
2024-11-24 22:57:18 -0300

    feat: 7 maze implemented

--- src/main.py
+++ src/main.py
@@ -1,23 +1,11 @@
 from src.models.window import Window
-from src.models.line import Line
-from src.models.point import Point
-from src.models.cell import Cell
+from src.models.maze import Maze
 
 
 def main():
     print("hello word")
     win = Window(800, 600)
-    cell1 = Cell(win, 20, 20, 40, 40, rw=False, bw=False)
-    cell2 = Cell(win, 40, 20, 60, 40, lw=False)
-    cell3 = Cell(win, 20, 40, 40, 60, tw=False, bw=False)
-    cell4 = Cell(win, 20, 60, 40, 80, tw=False)
-    cell1.draw()
-    cell2.draw()
-    cell3.draw()
-    cell4.draw()
-    cell2.draw_move(cell1)
-    cell1.draw_move(cell3)
-    cell3.draw_move(cell4)
+    maze = Maze(win, 50, 50, 14, 10, 50, 50)
     win.wait_for_close()
 
 
--- src/models/maze.py
+++ src/models/maze.py
@@ -1,10 +1,13 @@
+import time
+
 from src.models.window import Window
-from src.models.line import Line
-from src.models.point import Point
+from src.models.cell import Cell
 
 
 class Maze:
-    def __init__(self, win: Window, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y):
+    def __init__(
+        self, win: Window, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y
+    ):
         self.win = win
         self.x1 = x1
         self.y1 = y1
@@ -12,13 +15,27 @@ class Maze:
         self.num_cols = num_cols
         self.cell_size_x = cell_size_x
         self.cell_size_y = cell_size_y
+        self._cells = None
         self._create_cells()
-        
+
     def _create_cells(self):
-        pass
-    
+        self._cells = [[None for _ in range(self.num_cols)] for _ in range(self.num_rows)]
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+                self._draw_cell(i, j)
+
     def _draw_cell(self, i, j):
-        pass
-    
+        cell = Cell(
+                        self.win,
+                        (i + 1) * self.cell_size_x,
+                        (j + 1) * self.cell_size_y,
+                        (i + 2) * self.cell_size_x,
+                        (j + 2) * self.cell_size_y,
+                    )
+        self._cells[i][j] = cell
+        cell.draw()
+        self._animate()
+
     def _animate(self):
-        pass
+        self.win.redraw()
+        time.sleep(0.02)
